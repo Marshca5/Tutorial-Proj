6bab14c2fc5c9231de4fac35e8b07c758c0aa43d
Marshca5 <34908332+Marshca5@users.noreply.github.com>
2018-03-18 20:15:05 -0600

    Hello World! w/ 10x the power

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		//Prints out "Hello World!" (without quotations)
-		System.out.println("Hello World!");
+		//Prints out "Hello World!" (without quotations) 10 times
+		for(int i = 0; i < 10; i++)
+			System.out.println("Hello World!");
 	}
 
 }
